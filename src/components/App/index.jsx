3fc043b54f34f0367fee15041d91bf82d7416038
Tiago_Fugishima <tiago.fugishima@gmail.com>
2021-01-06 21:32:39 -0400

    Commit 3 - Criação da série em destaque e suas informações.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,6 +3,7 @@ import './style.css'
 
 import TMDb from '../../connection/TMDb'
 
+import Spotlight from '../Spotlight/index'
 import Row from '../Row/index'
 
 const App = () => {
@@ -17,8 +18,28 @@ const App = () => {
         loadItems()
     }, [])
 
+    // Função que define e carrega o item em destaque.
+    const [ spotlightData, setSpotlightData ] = useState(null)
+    useEffect(() => {
+        const loadSpotlight = async () => {
+            let list = await TMDb.getList()
+            let series = list.filter(i => i.slug === 'serie')
+            let randomizer = Math.floor(Math.random() * (series[0].items.results.length - 1))
+            let chosen = series[0].items.results[randomizer]
+            let chosenInfo = await TMDb.getInfo(chosen.id, 'tv')
+
+            setSpotlightData(chosenInfo)
+        }
+
+        loadSpotlight()
+    }, [])
+
     return (
         <div>
+            {spotlightData &&
+            <Spotlight item={spotlightData}/>
+            }
+
             <section className="lists">
                 {genreList.map((item, key) => (
                     <Row key={key} title={item.title} items={item.items}/>
--- src/components/Spotlight/index.jsx
+++ src/components/Spotlight/index.jsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import './style.css'
 
-const Spotlight = () => {
-    return (
-        <section>
+const Spotlight = ({item}) => {
+    // Variável que armazena a data de lançamento.
+    let releaseDate = new Date(item.first_air_date)
+    // Variável que armazena a sinopse.
+    let overview = item.overview
+    if(overview.length > 200) {
+        overview = overview.substring(0,200) + '...'
+    }
+    // Variável que armazena a lista de gêneros do item em destaque.
+    let genres = []
+    for(let i in item.genres) {
+        genres.push(item.genres[i].name)
+    }
 
+    return (
+        <section className="spotlight" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path}`
+        }}>
+            <div className="vertical">
+                <div className="horizontal">
+                    <div className="name">
+                        {item.name}
+                    </div>
+
+                    <div className="info">
+                        <div className="points">
+                            {item.vote_average} pontos
+                        </div>
+                        <div className="year_release">
+                            {releaseDate.getFullYear()}
+                        </div>
+                        <div className="seasons">
+                            {item.number_of_seasons} temporada{item.number_of_season !== 1 ? 's' : ''}
+                        </div>
+                    </div>
+
+                    <div className="overview">
+                        {overview}
+                    </div>
+
+                    <div className="buttons">
+                        <a href={`/watch/${item.id}`} className="watch_btn">
+                            ▶ Assistir
+                        </a>
+                        <a href={`/list/add/${item.id}`} className="add_btn">
+                            + Minha Lista
+                        </a>
+
+                        <div className="genres">
+                            Gêneros: <strong>{genres.join(', ')}</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
