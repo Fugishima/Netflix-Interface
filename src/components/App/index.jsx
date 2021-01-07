fe9ca019afdfd379f9f460dc64b559a7022f6178
Tiago_Fugishima <tiago.fugishima@gmail.com>
2021-01-06 21:28:15 -0400

    Commit 2 - Criação das listas de séries e filmes.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
+import TMDb from '../../connection/TMDb'
+
+import Row from '../Row/index'
+
 const App = () => {
+    // Função que carrega as listas de séries e filmes.
+    const [ genreList, setGenreList ] = useState([])
+    useEffect(() => {
+        const loadItems = async () => {
+            let list = await TMDb.getList()
+            setGenreList(list)
+        }
+
+        loadItems()
+    }, [])
+
     return (
         <div>
-
+            <section className="lists">
+                {genreList.map((item, key) => (
+                    <Row key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
         </div>
     )
 }
--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
-const Row = () => {
-    return (
-        <div>
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
+const Row = ({title, items}) => {
+    // Funções reponsáveis em movimentar as listas para direita e esquerda.
+    const [ scrollX,setScrollX ] = useState(0)
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2)
+        if(x > 0) {
+            x = 0
+        }
+        setScrollX(x)
+    }
+    const handleRighttArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2)
+        let listWidth = items.results.length * 150
+        if(window.innerWidth - listWidth > x) {
+            x = window.innerWidth - listWidth - 60
+        }
+
+        setScrollX(x)
+    }
+
+    return (
+        <div className="row">
+            <h2>
+                {title}
+            </h2>
+
+            <div className="left_arrow" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{fontSize: 50}}/>
+            </div>
+            <div className="right_arrow" onClick={handleRighttArrow}>
+                <NavigateNextIcon style={{fontSize: 50}}/>
+            </div>
+
+            <div className="list_area">
+                <div className="list" style={{marginLeft: scrollX, width: items.results.length * 150}}>
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className="item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.title}/>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
